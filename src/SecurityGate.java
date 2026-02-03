1b232b69215776f529f4b7d79a3243a875f93074
Karthik <vkarthik1900@gmail.com>
2026-02-03 13:40:48 +0530

    security class

--- src/SecurityGate.java
+++ src/SecurityGate.java
@@ -1,13 +1,20 @@
+import java.util.*;
 public class SecurityGate {
 
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {
         //1. Variable Declaration and Initialization
-        String passengerName = "Karthik";
-        int passengerAge = 22;
-        double passengerBaggageWeight = 18.5;
-        boolean passengerHasBoardingPass = true;
-        char gender = 'M';
+        System.out.println("Enter Name: ");
+        String passengerName = sc.nextLine();
+        System.out.println("Enter Age: ");
+        int passengerAge = sc.nextInt();
+        System.out.println("Enter Baggage Weight: ");
+        double passengerBaggageWeight = sc.nextDouble();
+        System.out.println("Boarding Pass Available (Trur/False): ");
+        boolean passengerHasBoardingPass = sc.nextBoolean();
+        System.out.println("Enter Gender (M/F): ");
+        char gender = sc.next().charAt(0);
 
         System.out.println("Processing Passenger: " + passengerName);
         System.out.println("Checking Baggage Weight: " + passengerBaggageWeight + "kg");
